9f8d8aa56f851b84cf42beb74f22f42bc4ffd8e5
Johan Carlsson <37298485+joaxcar@users.noreply.github.com>
2021-12-01 20:59:12 +0100

    Update Main.hs

--- day01/Main.hs
+++ day01/Main.hs
@@ -1,9 +1,7 @@
--- parse data
-getInputRaw path = do readFile path
-getInputLines path = do lines <$> getInputRaw path
-getInputInts path = do map (\x -> read x ::Int) <$> getInputLines path
+module Day6.Main where
 
--- run IO
+import AOC
+
 main = do {
 x <- getInputInts "data.txt";
 print $ [solution1 x, solution2 x]}
@@ -17,8 +15,5 @@ countIncrements x = sum $ map bool2int $ isIncrementing x
 sumWindow n x =  if (length xs) >= n
   then (sum $ take n x) : sumWindow n (drop 1 x)
   else []
-
-bool2int True = 1
-bool2int False = 0
 
 isIncrementing x = zipWith (<) x (drop 1 x
