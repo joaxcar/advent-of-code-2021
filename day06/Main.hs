f7dd2bb68e7c0466ba815df8c7125b24176a196f
Johan Carlsson <37298485+joaxcar@users.noreply.github.com>
2021-12-07 08:52:32 +0100

    Update Main.hs

--- day06/Main.hs
+++ day06/Main.hs
@@ -1,33 +1,34 @@
 import Data.List
 import Text.Parsec
 import Text.Parsec.String (Parser)
-import Data.Functor
 import Data.Sequence (Seq( (:<|) ), fromList, (|>))
 
-getInputRaw path = readFile path
-getInputLines path = lines <$> getInputRaw path
+import AOC
 
 main = do
-  input <- getInputRaw "d.txt"
-  let prog =  getRight  $ parse cmds "" input
-  let hej = init progtoFisht $ map (\x -> (length x) - 1) $ group . sort $ concat [[0..8],prog]
-  print $ sumF $ head$ drop 255 (step hej)
+  input <- getInputRaw "test.txt"
+  let prog =  getRight $ parse cmds "" input
+  let shoal = initShoal prog
+  print $ sumS $ shoal `getAt` 256
 
-sumF (F seq (x, y)) = (sum seq) + (x + y)
-toFish l = F (fromList $ take 7 l) (pair $ drop 7 l)
-  where pair (x : y : _) = (x, y)
+-- Model
+data Shoal = S (Seq Int) (Int, Int) deriving (Show)
 
-data Fish = F (Seq Int) (Int, Int) deriving (Show)
+-- Not very pretty but works
+initShoal prog = toShoal $ map (\x -> (length x) - 1) $ group . sort $ concat [[0..8],prog]
+  where
+    toShoal l = S (fromList $ take 7 l) (pair $ drop 7 l)
+    pair (x : y : _) = (x, y)
 
-step (F (x :<| rest) (s, e)) =
-   let update = F (rest |> (x + s)) (e, x)
-   in update : step update
-   
+-- Helpers
+getAt shoal day = head $ drop day (step shoal)
+sumS (S seq (x, y)) = (sum seq) + (x + y)
+
+-- Infinite list
+step state = state : step (next state)
+   where
+    next (S (x :<| rest) (s, e)) = S (rest |> (x + s)) (e, x)
 
 -- parser
-num = (\x -> read x :: Int) <$> many1 digit
-   
+num = (\x -> read x :: Int) <$> many1 digit 
 cmds = sepBy num (char ',')
-
-getRight (Right a) = a
-getRight (Left _) = []
