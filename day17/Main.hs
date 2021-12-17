80d386177861259dda2fa755c0be4d15bbf2b426
Johan Carlsson <37298485+joaxcar@users.noreply.github.com>
2021-12-17 23:53:41 +0100

    Update Main.hs

--- day17/Main.hs
+++ day17/Main.hs
@@ -7,24 +7,17 @@ import AOC
 -- input target area: x=102..157, y=-146..-90
 -- part 1: sum from 0 to abs of y min minus 1
 main = do
-  let move = mover target 157 (-146)
+  let move = mover targets 157 (-146)
   let part1 = sum [1..145]
   let part2 = length $ filter id $ map move starts
   print (part1, part2)
 
 starts = [((x,y), (0,0)) | x <- [7..157], y <- [-146..145]]
-
-target = M.fromList [((x,y),1) | x <- [102..157], y <- [-146..(-90)]]
+targets = M.fromList [((x,y),1) | x <- [102..157], y <- [-146..(-90)]]
 
 mover :: Map (Int, Int) Int -> Int -> Int -> ((Int,Int), (Int, Int)) -> Bool
-mover t mx my ((vx,vy), (x,y)) = 
-  if x > mx || y < my
-  then False
-  else if M.member (x,y) t
-  then True
-  else mover t mx my ((toZ vx, vy - 1), (x + vx, y + vy))
-
-toZ x
-  | x == 0 = 0
-  | x > 0 = x - 1
-  | x < 0 = x + 1
+mover t mx my ((vx,vy), (x,y))
+  | x > mx || y < my    = False
+  | M.member (x,y) t = True
+  | x == 0 = mover t mx my ((0, vy - 1), (x + vx, y + vy))
+  | otherwise = mover t mx my ((vx - 1, vy - 1), (x + vx, y + vy))
